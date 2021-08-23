add0d9e19443c31786bd102678bf758a549463da
Luis Miguel <luismiguelsilvaamorim@gmail.com>
2021-08-23 13:31:37 -0300

    Refactoring

--- index.js
+++ index.js
@@ -9,28 +9,27 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 
 // JWT
-const JWTSecret = '#####'
+const JWTSecret = 'sdadsaafadsfdsafdssadsjfaaasfsafsa'
 
 function auth(req, res, next){
     const authToken = req.headers['authorization']
 
-    if(authToken != undefined){
-        const bearer = authToken.split(' ')
-        const token = bearer[1]
-
-        jwt.verify(token, JWTSecret, (error, data) => {
-            if(error){
-                res.statusCode = 401
-                res.json({error: 'Token inválido'})
-            }else{
-                req.token = token
-                req.loggedUser = {id: data.id, email: data.email}
-                next()
-            }
-        })
-    }else{
-        res.sendStatus(401)
+    if(authToken == undefined){
+        return res.status(401).json({error: 'Token não autorizado'})
     }
+
+    const bearer = authToken.split(' ')
+    const token = bearer[1]
+
+    jwt.verify(token, JWTSecret, (error, data) => {
+        if(error){
+            return res.status(401).json({error: 'Token inválido'})
+        }
+
+        req.token = token
+        req.loggedUser = {id: data.id,email: data.email}
+        next()
+    })
 }
 
 // cors
@@ -65,16 +64,16 @@ app.get('/persons', auth, async (req, res) => {
 
     try{
         let people = await Person.findAll()
-        res.statusCode = 200
-        res.json({people, _links: HATEOAS})
+
+        res.status(200).json({people, _links: HATEOAS})
     }catch{
-        res.status(404)
+        return res.status(404).json('Não há pessoas cadastradas')
     }
 })
 
 app.get('/person/:id', auth, async (req, res) => {
     let id = req.params.id
-
+    
     let HATEOAS = [
         {
             href: 'http://localhost:8081/person',
@@ -82,12 +81,12 @@ app.get('/person/:id', auth, async (req, res) => {
             rel: 'post_person'
         },
         {
-            href: 'http://localhost:8081/person',
+            href: 'http://localhost:8081/person/'+id,
             method: 'DELETE',
             rel: 'delete_person'
         },
         {
-            href: 'http://localhost:8081/person',
+            href: 'http://localhost:8081/person/'+id,
             method: 'PUT',
             rel: 'put_person'
         },
@@ -98,175 +97,209 @@ app.get('/person/:id', auth, async (req, res) => {
         }
     ]
 
-    try{
-        if(isNaN(id)){
-            res.sendStatus(400)
-        }else{
-            let person = await Person.findOne({
-                where: {
-                    id
-                }
-            })
-            if(person == undefined || person == null){
-                res.sendStatus(404)
-            }else{
-                res.statusCode = 200
-                res.json({person, _links: HATEOAS})
-            }
-        }
-    }catch{
-        res.sendStatus(404)
+    if(!id){
+        return res.status(400).json({error: 'Id inválido'})
     }
+
+    let person = await Person.findOne({
+        where: {
+            id
+        }
+    })
+
+    if(!person){
+        return res.status(404).json({error: 'Pessoa não cadastrada'})
+    }
+
+    res.status(200).json({person, _links: HATEOAS})
 })
 
 app.post('/person', auth, async (req, res) => {
-    let name = req.body.name
-    let age = req.body.age
+    let { name, age } = req.body
 
-    try{
-        if(name == undefined || age == undefined || age < 0){
-            res.sendStatus(400)
-        }else{
-            await Person.create({name, age})
-            res.sendStatus(200)
-        }
-    }catch{
-        res.sendStatus(500)
+    if(!name || !age || age < 0){
+        return res.status(400).json({error: 'Dados inválidos'})
     }
+
+    let person = await Person.create({name, age})
+
+    if(!person){
+        return res.status(500).json({error: 'Erro interno ao cadastrar pessoa'})
+    }
+
+    res.status(200).json({success: 'Pessoa cadastrada'})
+
 })
 
 app.delete('/person/:id', auth, async (req, res) => {
     let id = req.params.id
 
-    try{
-        if(isNaN(id)){
-            res.sendStatus(400)
-        }else{
-            let person = await Person.destroy({
-                where:{
-                    id
-                }
-            })
-
-            if(person == 0){
-                res.sendStatus(404)
-            }else if(person == 1){
-                res.sendStatus(200)
-            }
-        }
-    }catch{
-        res.sendStatus(404)
+    if(!id){
+        return res.status(400).json({error: 'Id inválido'})
     }
+
+    let person = await Person.destroy({
+        where: {
+            id
+        }
+    })
+
+    if(!person){
+        return res.status(404).json({error: 'Pessoa não encontrada'})
+    }
+
+    res.status(200).json({success: 'Pessoa excluída'})
 })
 
 app.put('/person/:id', auth, async (req, res) => {
     let id = req.params.id
-    let name = req.body.name
-    let age = req.body.age
+    let { name, age } = req.body
 
-    try{
-        if(isNaN(id)){
-            res.sendStatus(400)
-        }else{
-            let person = await Person.update({name, age}, {
-                where: {
-                    id
-                }
-            })
-            
-            if(person == 0){
-                res.sendStatus(404)
-            }else if(person == 1){
-                res.sendStatus(200)
-            }
-        }
-    }catch{
-        res.sendStatus(404)
+    if(!id){
+        return res.status(400).json({error: 'Id inválido'})
     }
+
+    let person = await Person.update({name, age}, {
+        where: {
+            id
+        }
+    }) 
+
+    if(!person){
+        return res.status(404).json({error: 'Pessoa não encontrada'})
+    }
+
+    res.status(200).json({success: 'Pessoa editada'})
 })
 
 // user
 app.get('/users', auth, async (req, res) => {
-    try{
-        let users = await User.findAll()
-        res.statusCode = 200
-        res.json(users)
-    }catch{
-        res.sendStatus(404)
+
+    let HATEOAS = [
+        {
+            href: 'http://localhost:8081/user/0',
+            method: 'GET',
+            rel: 'get_user'
+        },
+        {
+            href: 'http://localhost:8081/auth',
+            method: 'POST',
+            rel: 'login'
+        }
+    ]
+
+    let users = await User.findAll()
+    
+    if(!users){
+        return res.sendStatus(404).json({error: 'Não há usuários cadastrados'})
     }
+
+    res.status(200).json({users, _links: HATEOAS})
 })
 
-app.post('/user', async (req, res) => {
-    let name = req.body.name
-    let email = req.body.email
-    let password = req.body.password
+app.get('/user/:id', auth, async (req, res) => {
+    let id = req.params.id
 
+    let HATEOAS = [
+        {
+            href: 'http://localhost:8081/user/'+id,
+            method: 'POST',
+            rel: 'post_user'
+        },
+        {
+            href: 'http://localhost:8081/user/'+id,
+            method: 'DELETE',
+            rel: 'delete_user'
+        },
+        {
+            href: 'http://localhost:8081/auth',
+            method: 'POST',
+            rel: 'login'
+        }
+    ]
+
+    if(!id){
+        return res.status(400).json({error: 'Id inválido'})
+    }
+
+    let user = await User.findOne({
+        where: {
+            id
+        }
+    })
+
+    if(!user){
+        return res.status(404).json({error: 'Usuário não encontrado'})
+    }
+
+    res.status(200).json({user, _link: HATEOAS})
+})
+
+app.post('/user', auth, async (req, res) => {
+    let { name, email, password } = req.body
     let salt = bcrypt.genSaltSync(10)
     let hash = bcrypt.hashSync(password, salt)
 
-    try{
-        if(name == undefined || email == undefined || password == undefined){
-            res.sendStatus(400)
-        }else{
-            await User.create({name, email, password: hash})
-            res.sendStatus(200)
-        }
-    }catch{
-        res.sendStatus(500)
+    if(!name || !email || !password){
+        return res.status(400).json({error: 'Dados inválidos'})
     }
+
+    let user = User.create({name, email, password: hash})
+
+    if(!user){
+        return res.status(500).json({error: 'Erro interno ao cadastrar usuário'})
+    }
+
+    res.status(200).json({success: 'Usuário cadastrado'})
 })
 
 app.delete('/user/:id', auth, async (req, res) => {
     let id = req.params.id
 
-    try{
-        let user = await User.destroy({
-            where: {
-                id
-            }
-        }) 
-
-        if(!user){
-            res.sendStatus(404)
+    let user = await User.destroy({
+        where: {
+            id
         }
+    })
 
-        res.sendStatus(200)
-    }catch{
-        res.sendStatus(500)
+    if(!user){
+        return res.status(404).json({error: 'Usuário não encontrado'})
     }
+
+    res.status(200).json({success: 'Usuário deletado'})
 })
 
 app.post('/auth', async (req, res) => {
-    let email = req.body.email
-    let password = req.body.password
+    let { email, password } = req.body
 
-    if(email == undefined){
-        res.sendStatus(400)
-    }else{
-        let user = await User.findOne({
-            where: {
-                email
-            }
-        })
-        if(user == undefined){
-            res.sendStatus(404)
-        }else{
-            let validate = bcrypt.compareSync(password, user.password)
-
-            if(validate){
-                jwt.sign({id: user.id, email: user.email}, JWTSecret, {expiresIn: '48h'}, (error, token) => {
-                    if(error){
-                        res.sendStatus(400)
-                    }else{
-                        res.statusCode = 200
-                        res.json({token})
-                    }
-                })
-            }else{
-                res.sendStatus(401)
-            }
-        }
+    if(!email){
+        return res.status(400).json({error: 'Email inválido'})
     }
+
+    let user = await User.findOne({
+        where: {
+            email
+        }
+    })
+
+    if(!user){
+        return res.status(404).json({error: 'Usuário não cadastrado'})
+    }
+
+    let validate = bcrypt.compareSync(password, user.password)
+
+    if(!validate){
+        return res.status(401).json({error: 'Credenciais inválidas'})
+    }
+    
+    jwt.sign({id: user.id, email: user.email}, JWTSecret, {expiresIn: '48h'}, (error, token) => {
+
+        if(error){
+            return res.status(400).json({error: 'Credenciais inválidas'})
+        }
+
+        res.status(200).json({token})
+    })
 })
 
 const port = 8081
